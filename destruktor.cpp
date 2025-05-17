f83f64b19863e2cecd3d3b68356f5558914150bd
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-17 16:19:11 +0700

    last destruktor

--- destruktor.cpp
+++ destruktor.cpp
@@ -24,3 +24,24 @@ angka::angka(){//Destructor
     delete[]arr;
     cout<<"Alamat Array Sudah Dilepaskan" <<endl;
 }
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = ";cin>>arr[i];
+    }
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = ";cin>>arr[i];
+    }
+    cout<<endl;
+}
+
+int main(){
+    angka belajarcpp(3); //Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka (5); //Constructor Dipanggil
+    delete ptrBelajarcpp; //Destructor Dipanggil
+
+    return 0;
+}//Destructor Dipanggil
