742bb4894c7b0d62c648e0ebfcaa23f0a9f2e113
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-17 17:29:47 +0700

    last mbrStatikfunc

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -12,7 +12,7 @@ public:
     void setID();
     void printA11();
 
-    static void setNimm(int pNim){nim=pNim; /*Definisi Function*/}
+    static void setNim(int pNim){nim=pNim; /*Definisi Function*/}
     static int getNim(){return nim; /*Definisi Function*/}
 
     mahasiswa(string pnama):nama(pnama){setID();}
@@ -28,4 +28,20 @@ void mahasiswa::printA11(){
     cout<<"ID   = "<<id<<endl;
     cout<<"Nama = "<<nama<<endl;
     cout<<endl;
+}
+
+int main(){
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printA11();
+    mhs2.printA11();
+    mhs3.printA11();
+    mhs4.printA11();
+    cout<<"akses dari luar object = "<<mahasiswa::getNim()<<endl; //mengakses nim melalui static member function "getNim"
+
+    return 0;
 }
