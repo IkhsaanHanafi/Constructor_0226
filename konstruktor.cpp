ed83a77166e68a7383c88112442e3df934195b1a
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-17 16:30:36 +0700

    add konstruktor

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -9,5 +9,20 @@ string nama;
 public:
 mahasiswa();
 mahasiswa(int);
+mahasiswa(string);
+mahasiswa(int iNim, string iNama);
+void cetak();
+};
 
+mahasiswa :: mahasiswa(){
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa :: mahasiswa(int iNim){
+nim=iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+nama=iNama;
 }
